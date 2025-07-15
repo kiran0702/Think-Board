e617fa5b1f98d98bd0918bcc6f2dc6c3fa609929
kiran0702 <kiran7722p@gmail.com>
2025-07-16 02:49:47 +0530

    fix error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import .meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api"
-const api = axios.create(
-  {
-    baseURL: BASE_URL
-  }
-)
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "/api";
+
+const api = axios.create({
+  baseURL: BASE_URL
+});
 
 export default api;
